0d4bd08e7ff0d4de7e8130b460c336ef976c7104
chienhalan5000 <chienkakashi@gmail.com>
2018-11-28 22:03:10 +0700

    fix css

---
+++ asset/functions.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    // $("#destination").css({ "backgroundColor": "black", "color": "white" });
+    // $(".controls").wrapAll( '<div class="navication"></div>');
+    // $('.pac-container.pac-logo.hdpi').css({"z-index" : "999999999"});
+    // $('.tactile-searchbox-input').css({"color": "rgba(255,255,255,0.87)"});
+    
+});
